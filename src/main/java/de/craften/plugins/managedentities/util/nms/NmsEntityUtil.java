be57dc98a5752791848864a52dd11bc2fafcb517
leMaik <an@maik.tk>
2016-02-13 21:25:11 +0100

    Add a utility method to set the head rotation of a living entity.

--- src/main/java/de/craften/plugins/managedentities/util/nms/NmsEntityUtil.java
+++ src/main/java/de/craften/plugins/managedentities/util/nms/NmsEntityUtil.java
@@ -1,8 +1,10 @@
 package de.craften.plugins.managedentities.util.nms;
 
 import net.minecraft.server.v1_8_R3.Entity;
+import net.minecraft.server.v1_8_R3.EntityLiving;
 import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftLivingEntity;
 import org.bukkit.entity.LivingEntity;
 
 /**
@@ -41,5 +43,23 @@ public class NmsEntityUtil {
         nmsEntity.c(tag);
         tag.setInt("Invulnerable", invulnerable ? 1 : 0);
         nmsEntity.f(tag);
+    }
+
+    public static void setHeadRotation(LivingEntity entity, float yaw, float pitch) {
+        EntityLiving nmsEntity = ((CraftLivingEntity) entity).getHandle();
+        nmsEntity.f(clampYaw(yaw));
+        nmsEntity.g(pitch);
+    }
+
+    private static float clampYaw(float yaw) {
+        while (yaw < -180.0F) {
+            yaw += 360.0F;
+        }
+
+        while (yaw >= 180.0F) {
+            yaw -= 360.0F;
+        }
+
+        return yaw;
     }
 }
