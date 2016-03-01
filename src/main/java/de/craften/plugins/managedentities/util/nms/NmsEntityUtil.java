49204dc676a859c7cfb55ac348cc6abd0d52b784
leMaik <an@maik.tk>
2016-03-01 15:36:35 +0100

    Update to bukkit 1.9.

--- src/main/java/de/craften/plugins/managedentities/util/nms/NmsEntityUtil.java
+++ src/main/java/de/craften/plugins/managedentities/util/nms/NmsEntityUtil.java
@@ -1,10 +1,10 @@
 package de.craften.plugins.managedentities.util.nms;
 
-import net.minecraft.server.v1_8_R3.Entity;
-import net.minecraft.server.v1_8_R3.EntityLiving;
-import net.minecraft.server.v1_8_R3.NBTTagCompound;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftEntity;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftLivingEntity;
+import net.minecraft.server.v1_9_R1.Entity;
+import net.minecraft.server.v1_9_R1.EntityLiving;
+import net.minecraft.server.v1_9_R1.NBTTagCompound;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftEntity;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftLivingEntity;
 import org.bukkit.entity.LivingEntity;
 
 /**
@@ -30,10 +30,7 @@ public class NmsEntityUtil {
      */
     public static void setAi(LivingEntity entity, boolean hasAi) {
         Entity nmsEntity = ((CraftEntity) entity).getHandle();
-        NBTTagCompound tag = nmsEntity.getNBTTag();
-        if (tag == null) {
-            tag = new NBTTagCompound();
-        }
+        NBTTagCompound tag = new NBTTagCompound();
         nmsEntity.c(tag);
         tag.setInt("NoAI", hasAi ? 0 : 1);
         nmsEntity.f(tag);
@@ -48,10 +45,7 @@ public class NmsEntityUtil {
      */
     public static void setInvulnerable(LivingEntity entity, boolean invulnerable) {
         Entity nmsEntity = ((CraftEntity) entity).getHandle();
-        NBTTagCompound tag = nmsEntity.getNBTTag();
-        if (tag == null) {
-            tag = new NBTTagCompound();
-        }
+        NBTTagCompound tag = new NBTTagCompound();
         nmsEntity.c(tag);
         tag.setInt("Invulnerable", invulnerable ? 1 : 0);
         nmsEntity.f(tag);
@@ -67,7 +61,8 @@ public class NmsEntityUtil {
     public static void setHeadRotation(LivingEntity entity, float yaw, float pitch) {
         EntityLiving nmsEntity = ((CraftLivingEntity) entity).getHandle();
         nmsEntity.f(clampYaw(yaw));
-        nmsEntity.g(pitch);
+        //nmsEntity.g(pitch);
+        //TODO test
     }
 
     private static float clampYaw(float yaw) {
