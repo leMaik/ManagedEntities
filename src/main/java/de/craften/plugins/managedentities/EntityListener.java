bb97cc3962f0252c91a5c6a201738ea327e7bdc0
leMaik <an@maik.tk>
2016-05-07 00:25:31 +0200

    Don't unload chunks with managed entities.

--- src/main/java/de/craften/plugins/managedentities/EntityListener.java
+++ src/main/java/de/craften/plugins/managedentities/EntityListener.java
@@ -1,9 +1,11 @@
 package de.craften.plugins.managedentities;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
 
 class EntityListener implements Listener {
     private final EntityManager entityManager;
@@ -17,6 +19,16 @@ class EntityListener implements Listener {
         ManagedEntity entity = entityManager.getEntity(event.getEntity());
         if (entity instanceof ManagedEntityBase) {
             ((ManagedEntityBase) entity).onDeath();
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChunkUnload(ChunkUnloadEvent event) {
+        for (Entity entity : event.getChunk().getEntities()) {
+            if (entityManager.getEntity(entity) != null) {
+                event.setCancelled(true);
+                return;
+            }
         }
     }
 }
